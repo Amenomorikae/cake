086061fa7317297cf6a641fe88171dc58cf0bd23
amke <amenomorikae@amenomorikaenoMacBook-Pro.local>
2020-03-22 09:47:37 +0900

    2-2

--- app/controllers/end_users_controller.rb
+++ app/controllers/end_users_controller.rb
@@ -1,5 +1,28 @@
 class EndUsersController < ApplicationController
 	def show
+		@end_user = current_end_user
+	end
+
+	def edit
+	end
+
+    def update
+    	@end_user = EndUser.find(params[:id])
+	   	if @end_user.update(end_user_params)
+	       flash[:notice] = “successfully”
+	       redirect_to end_users_path
+	     else
+	      render :edit
+	    end
+    end
+
+	def destroy
+		@end_user = end_user.find_by(id: params[:id])
+        if @end_user.destroy
+            redirect_to root_path
+        else
+            render :edit
+        end
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,6 @@ Rails.application.routes.draw do
   passwords:     'admins/passwords',
   registrations: 'admins/registrations'
 }
-  devise_for :end_users,controllers: {
-  sessions:      'end_users/sessions',
-  passwords:     'end_users/passwords',
-  registrations: 'end_users/registrations'
-}
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -20,6 +14,12 @@ Rails.application.routes.draw do
   end
 
   resource :end_users,only: [ :show, :edit, :update, :confirm]
+
+  devise_for :end_users,controllers: {
+  sessions:      'end_users/sessions',
+  passwords:     'end_users/passwords',
+  registrations: 'end_users/registrations'
+}
 
   resources :tops,only: [:index]
 
